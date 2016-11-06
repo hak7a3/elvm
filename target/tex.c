c7c9bf4c6395ab0afb9465ff6d9b1e215a2bac1e
HAKUTA Shizuya <hak7a3@gmail.com>
2016-11-06 16:42:32 +0900

    must be string!

--- target/tex.c
+++ target/tex.c
@@ -40,14 +40,14 @@ static void tex_emit_inst(Inst* inst) {
   case ADD:
     emit_line("\\count0=\\@reg@%s\\relax", reg_names[inst->dst.reg]);
     emit_line("\\advance\\count0by%s\\relax", tex_src_str(inst));
-    emit_line("\\ifnum\\count0>%d\\advance\\count0by-%d\\fi", UINT_MAX, 16777216);
+    emit_line("\\ifnum\\count0>%d\\advance\\count0by-%s\\fi", UINT_MAX, "16777216");
     emit_line("\\edef\\@reg@%s{\\the\\count0}%%", reg_names[inst->dst.reg]);
     break;
 
   case SUB:
     emit_line("\\count0=\\@reg@%s\\relax", reg_names[inst->dst.reg]);
     emit_line("\\advance\\count0by-%s\\relax", tex_src_str(inst));
-    emit_line("\\ifnum\\count0<0\\advance\\count0by%d\\fi", 16777216);
+    emit_line("\\ifnum\\count0<0\\advance\\count0by%s\\fi", "16777216");
     emit_line("\\edef\\@reg@%s{\\the\\count0}%%", reg_names[inst->dst.reg]);
     break;
 
